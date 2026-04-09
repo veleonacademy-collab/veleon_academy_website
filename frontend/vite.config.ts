407f75c39b7b6e5f5d199dad33d14fe1eac6a213
omidoyin <omidoyinayodeji@yahoo.com>
2026-04-09 11:05:01 +0100

    Fix: Fix react splitting error casued by manual chunks function.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,16 +11,6 @@ export default defineConfig({
     cssCodeSplit: false,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react";
-            if (id.includes("lucide-react")) return "vendor-icons";
-            if (id.includes("@tanstack")) return "vendor-query";
-            return "vendor";
-          }
-        },
-      },
     },
   },
   server: {
